ccc2eaecb94426f2e5e363e072a258c77d3d638b
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-03-08 00:12:47 +0100

    add @Controller adnotation to class declaration

--- client/src/com/codecool/audioStreamClientUtils/UdpClient.java
+++ client/src/com/codecool/audioStreamClientUtils/UdpClient.java
@@ -1,11 +1,8 @@
 package com.codecool.audioStreamClientUtils;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
@@ -13,7 +10,7 @@ import java.net.MulticastSocket;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-@Service
+@Controller
 public class UdpClient {
 
     private final String GROUP_IP = "230.0.0.1";
@@ -57,53 +54,4 @@ public class UdpClient {
         }
     }
 
-    public static class Player implements Runnable {
-
-        AudioFormat format;
-        BlockingQueue<byte[]> input;
-
-        public AudioFormat getFormat() {
-            return format;
-        }
-
-        public Player setFormat(AudioFormat format) {
-            this.format = format;
-            return this;
-        }
-
-        public BlockingQueue<byte[]> getInput() {
-            return input;
-        }
-
-        public Player setInput(BlockingQueue<byte[]> input) {
-            this.input = input;
-            return this;
-        }
-
-        @Override
-        public void run() {
-
-            try {
-                SourceDataLine out = AudioSystem.getSourceDataLine(format);
-                out.open(format);
-    //            Line.Info info = out.getLineInfo();
-    //            Arrays.stream(AudioSystem.getMixer(AudioSystem.getMixerInfo()[0]).getSourceLines()).forEach(System.out::println);
-                out.start();
-    //            Arrays.stream(out.getControls()).forEach(System.out::println);
-    //            BooleanControl muter = (BooleanControl)out.getControl(BooleanControl.Type.MUTE);
-    //            FloatControl vol = (FloatControl)out.getControl(FloatControl.Type.MASTER_GAIN);
-    //            System.out.println(input.size());
-                while (out.isOpen()) {
-    //                if (vol.getValue() > vol.getMinimum())
-    //                    vol.setValue(vol.getValue() - 0.5f);
-                    out.write(input.take(), 0, (int) format.getFrameRate());
-    //                System.out.println(input.size());
-                }
-
-            } catch (LineUnavailableException | InterruptedException e ) {
-                e.printStackTrace();
-            }
-
-        }
-    }
 }
--- src/main/java/com/codecool/audioStream/ServerApp.java
+++ src/main/java/com/codecool/audioStream/ServerApp.java
@@ -1,15 +1,32 @@
 package com.codecool.audioStream;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sound.sampled.LineUnavailableException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ServerApp {
 
-    public void start() {
+    public static void main(String[] args) {
 
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext("com.codecool.audioStream");
+        UdpServer udpServer = (UdpServer) context.getBean("UdpServer");
+        try {
+            InputStream in = new FileInputStream("/resources/sample.wav");
+            udpServer.setIn(in);
+            udpServer.start();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
 
     }
 }
