b0ac335fbce701f5096d8828eab53853efab5758
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-03-08 14:11:46 +0100

    Add testing prints to run method

--- src/main/java/com/codecool/audioStream/ConnectionListener.java
+++ src/main/java/com/codecool/audioStream/ConnectionListener.java
@@ -14,19 +14,22 @@ public class ConnectionListener implements Runnable{
     private boolean running;
     private DatagramSocket socket;
 
-    public ConnectionListener(ConcurrentSkipListSet set) {
+    public ConnectionListener(ConcurrentSkipListSet<SocketAddress> set) {
         this.set = set;
+
     }
 
     @Override
     public void run() {
         running = true;
+        System.out.println("run");
         try {
             socket = new DatagramSocket(LISTENING_PORT);
             while (running){
                 byte[] bf = new byte[1];
                 DatagramPacket packet = new DatagramPacket(bf, bf.length);
                 socket.receive(packet);
+                System.out.println(packet.getAddress().toString());
                 set.add(packet.getSocketAddress());
             }
         } catch (IOException e) {
